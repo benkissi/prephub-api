8c433a0b1fb9216bf037c56eb0a094b7ae172844
Ben Kissi <ben@benkissi.com>
2020-07-16 14:50:51 +0000

    add load more route

--- routes/torre.js
+++ routes/torre.js
@@ -8,19 +8,53 @@ const { TORRE } = require("../utils/endpoints");
 router.post("/matching", auth, async (req, res) => {
   try {
     const { username } = req.body;
-    console.log("username", username);
+
     const url = TORRE.GET_USER(username);
+    const jobsURL = TORRE.GET_JOBS(0, 10, true);
+
     const user = await axios.get(url);
-    console.log("user", user.data);
+    const jobs = await axios.post(
+      jobsURL,
+      {},
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
     const data = {
-      bio: user.data
-    }
-    res.send(data)
+      bio: user.data,
+      jobs: jobs.data,
+    };
+    res.send(data);
   } catch (error) {
     console.log(error);
     return res.status(400).json(error);
   }
 });
 
+router.post("/next-jobs", auth, async (req, res) => {
+  try {
+    const { offset, size, aggregate, strengths } = req.body;
+    console.log(offset, size, aggregate)
 
-module.exports = router
+    const jobsURL = TORRE.GET_JOBS(offset, size, aggregate);
+    const jobs = await axios.post(
+      jobsURL,
+      {},
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    
+    res.send(jobs.data);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json(error);
+  }
+});
+
+module.exports = router;
--- utils/endpoints.js
+++ utils/endpoints.js
@@ -1,5 +1,6 @@
 const TORRE = {
-    GET_USER: (username) => `https://torre.bio/api/bios/${username}`
+    GET_USER: (username) => `https://torre.bio/api/bios/${username}`,
+    GET_JOBS: (offset, size, aggregate) => `https://search.torre.co/opportunities/_search/?offset=${offset}&size=${size}&aggregate=${aggregate}`
 }
 
 module.exports = {
