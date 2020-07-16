b30a200cd64d593112788f264d0e5ab70fc53841
Ben Kissi <ben@benkissi.com>
2020-07-16 10:29:54 +0000

    set dynamic port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const authRoutes = require('./routes/auth')
 const torreRoutes = require('./routes/torre')
 
 const app = express()
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 
 app.use(cors())
 app.use(bodyParser.json());
